bdf943e7b7b371e9a68e7270e971729403fef6b8
Mingguriguri <rlaalswjd-@naver.com>
2024-04-11 18:31:36 +0900

    RESET the DEV branch

--- restfulApiPractice/src/main/java/com/minjeong/restfulapipractice/dto/RequestBodyDTO.java
+++
@@ -1,9 +0,0 @@
-package com.minjeong.restfulapipractice.dto;
-
-import lombok.Data;
-
-@Data // Data에 Getter Setter 다 있음!
-public class RequestBodyDTO {
-    private int std_id;
-    private String name;
-}
