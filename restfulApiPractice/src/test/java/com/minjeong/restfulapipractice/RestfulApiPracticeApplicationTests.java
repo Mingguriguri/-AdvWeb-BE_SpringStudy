caa2f0966df6b8d2081429fd7ff2a342ad73dd8c
Mingguriguri <rlaalswjd-@naver.com>
2024-04-11 18:28:01 +0900

    InClassPractice: RestfulAPI를 이용해서 학번과 이름을 반환해주는 프로젝트 만들기

---
+++ restfulApiPractice/src/test/java/com/minjeong/restfulapipractice/RestfulApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minjeong.restfulapipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
