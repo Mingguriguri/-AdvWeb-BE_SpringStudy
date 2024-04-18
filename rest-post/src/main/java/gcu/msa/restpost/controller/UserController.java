ad46990aa59e672dcd56501bf2b7c0fbd3ccf8a6
Mingguriguri <rlaalswjd-@naver.com>
2024-04-18 16:06:33 +0900

    feat: SwaggerUI에 APIResponse 추가하기

--- rest-post/src/main/java/gcu/msa/restpost/controller/UserController.java
+++ rest-post/src/main/java/gcu/msa/restpost/controller/UserController.java
@@ -2,6 +2,9 @@ package gcu.msa.restpost.controller;
 
 
 import gcu.msa.restpost.domain.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +12,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping
 @Tag(name="gcu swagger", description = "gcu management APIs")
+@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK!!"),
+        @ApiResponse(responseCode = "400", description = "BAD REQUEST!!"),
+        @ApiResponse(responseCode = "404", description = "NOT FOUND!!"),
+        @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR!!")
+})
 public class UserController {
 
     @GetMapping("/hello")
