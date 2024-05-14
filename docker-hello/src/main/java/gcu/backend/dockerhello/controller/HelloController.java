ca065595b9fc17c0d671f73fb9e1e41ef54cbfb8
Mingguriguri <rlaalswjd-@naver.com>
2024-05-14 15:08:05 +0900

    W10 - docker 실행시켜보기(Dockerfiile -> build docker image -> run docker container)

---
+++ docker-hello/src/main/java/gcu/backend/dockerhello/controller/HelloController.java
@@ -0,0 +1,14 @@
+package gcu.backend.dockerhello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloController {
+    @GetMapping("/hello")
+    public String getMessage(){
+        return "Hello World";
+    }
+}
