e32b8be543c5cc38d85b66d4bc4abd61390477ed
Mingguriguri <rlaalswjd-@naver.com>
2024-05-14 13:20:51 +0900

    W10 - create hello docker project

---
+++ docker-hello/src/test/java/gcu/backend/dockerhello/DockerHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package gcu.backend.dockerhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
